e9463be678d74740f5fda40d5e1f4ea2556776f8
Yuya KITANO <uyaspecial@gmail.com>
2024-05-20 17:26:06 +0900

    Add 'Joukowsky-airfoil.html' download

--- app/Joukowsky-airfoil.js
+++ app/Joukowsky-airfoil.js
@@ -149,6 +149,14 @@ function exportModel(form_id, plot_id, width, height)
 	//-------------------------------------------------
 	(new CSV(arr)).save('Joukowsky-airfoil.csv');
 	//-------------------------------------------------
+	const link = document.createElement("a");
+	// -----
+	link.href = './app/PyVista/Joukowsky-airfoil.html';
+	link.download = 'Joukowsky-airfoil.html';
+	link.click();
+	// -----
+	link.remove();
+	//-------------------------------------------------
 	updateModel(form_id, plot_id, width, height);
 };
 //==================================================================
@@ -305,86 +313,86 @@ download = function (data, fileName){
 //==================================================================
 class CSV {
 	constructor(data, keys = false) {
-	this.ARRAY	= Symbol('ARRAY')
-	this.OBJECT = Symbol('OBJECT')
-	
-	this.data = data
-	
-	if (CSV.isArray(data)) {
-		if (0 == data.length) {
-		this.dataType = this.ARRAY
-		} else if (CSV.isObject(data[0])) {
-		this.dataType = this.OBJECT
-		} else if (CSV.isArray(data[0])) {
-		this.dataType = this.ARRAY
+		this.ARRAY	= Symbol('ARRAY')
+		this.OBJECT = Symbol('OBJECT')
+		
+		this.data = data
+		
+		if (CSV.isArray(data)) {
+			if (0 == data.length) {
+			this.dataType = this.ARRAY
+			} else if (CSV.isObject(data[0])) {
+			this.dataType = this.OBJECT
+			} else if (CSV.isArray(data[0])) {
+			this.dataType = this.ARRAY
+			} else {
+			throw Error('Error: 未対応のデータ型です')
+			}
 		} else {
-		throw Error('Error: 未対応のデータ型です')
+			throw Error('Error: 未対応のデータ型です')
 		}
-	} else {
-		throw Error('Error: 未対応のデータ型です')
-	}
-	
-	this.keys = keys
+		
+		this.keys = keys
 	}
 	
 	toString() {
-	if (this.dataType === this.ARRAY) {
-		return this.data.map((record) => (
-		record.map((field) => (
-			CSV.prepare(field)
-		)).join(',')
-		)).join('\n')
-	} else if (this.dataType === this.OBJECT) {
-		const keys = this.keys || Array.from(this.extractKeys(this.data))
-		
-		const arrayData = this.data.map((record) => (
-		keys.map((key) => record[key])
-		))
-		
-		console.log([].concat([keys], arrayData))
-		
-		return [].concat([keys], arrayData).map((record) => (
-		record.map((field) => (
-			CSV.prepare(field)
-		)).join(',')
-		)).join('\n')
-	}
+		if (this.dataType === this.ARRAY) {
+			return this.data.map((record) => (
+			record.map((field) => (
+				CSV.prepare(field)
+			)).join(',')
+			)).join('\n')
+		} else if (this.dataType === this.OBJECT) {
+			const keys = this.keys || Array.from(this.extractKeys(this.data))
+			
+			const arrayData = this.data.map((record) => (
+			keys.map((key) => record[key])
+			))
+			
+			console.log([].concat([keys], arrayData))
+			
+			return [].concat([keys], arrayData).map((record) => (
+			record.map((field) => (
+				CSV.prepare(field)
+			)).join(',')
+			)).join('\n')
+		}
 	}
 	
 	save(filename = 'data.csv') {
-	if (!filename.match(/\.csv$/i)) { filename = filename + '.csv' }
-	
-	console.info('filename:', filename)
-	console.table(this.data)
-	
-	const csvStr_SJIS = this.toString()
-	
-	const blob = new Blob([csvStr_SJIS], {'type': 'text/csv'});
-	const url = window.URL || window.webkitURL;
-	const blobURL = url.createObjectURL(blob);
-	
-	var a = document.createElement('a');
-	a.download = decodeURI(filename);
-	a.href = blobURL;
-	a.type = 'text/csv';
-	
-	a.click();
+		if (!filename.match(/\.csv$/i)) { filename = filename + '.csv' }
+		
+		console.info('filename:', filename)
+		console.table(this.data)
+		
+		const csvStr_SJIS = this.toString()
+		
+		const blob = new Blob([csvStr_SJIS], {'type': 'text/csv'});
+		const url = window.URL || window.webkitURL;
+		const blobURL = url.createObjectURL(blob);
+		
+		var a = document.createElement('a');
+		a.download = decodeURI(filename);
+		a.href = blobURL;
+		a.type = 'text/csv';
+		
+		a.click();
 	}
 	
 	extractKeys(data) {
-	return new Set([].concat(...this.data.map((record) => Object.keys(record))))
+		return new Set([].concat(...this.data.map((record) => Object.keys(record))))
 	}
 	
 	static prepare(field) {
-	return '"' + (''+field).replace(/"/g, '""') + '"'
+		return '"' + (''+field).replace(/"/g, '""') + '"'
 	}
 	
 	static isObject(obj) {
-	return '[object Object]' === Object.prototype.toString.call(obj)
+		return '[object Object]' === Object.prototype.toString.call(obj)
 	}
 	
 	static isArray(obj) {
-	return '[object Array]' === Object.prototype.toString.call(obj)
+		return '[object Array]' === Object.prototype.toString.call(obj)
 	}
 }
 //==================================================================
