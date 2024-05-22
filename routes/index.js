6396d8e3032c5fa8bf6822d2e155b6d71d5ade6b
Yuya KITANO <uyaspecial@gmail.com>
2024-05-22 14:31:02 +0900

    Update Buttons

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const fs = require('fs');
+const subproc = require('child_process');
 
+// https://zenn.dev/wkb/books/node-tutorial/viewer/todo_03
 
 router.get("/", (req, res) => {
-  res.render("./index.ejs");
+    res.render("./index.ejs",{ fs, subproc } );
 });
+
+// router.post("/", (req, res) => {
+    // res.redirect('./index.ejs');
+	// console.log('##############################');
+// });
 
 module.exports = router;
